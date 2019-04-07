44d291114a368c2292e60740626803ae69180723
Mr-onions <mr-onions@Mr-onionsdeMacBook-Pro.local>
2019-04-07 14:31:32 +0800

    finish commit

--- test/test.js
+++ test/test.js
@@ -11,16 +11,16 @@ describe('regex', function () {
   })
 
   it('可以捕获正整数 x 的值，末尾带有 x 的变量不应当被捕获', function () {
-    const f = require('../lib/capture_breakword')
+        const f = require('../lib/capture_breakword')
 
-    assert.equal(f('x=5'), '5', 'x=5')
-    assert.equal(f('abc x=5'), '5', 'abc x=5')
-    assert.equal(f('fox=123'), null, 'fox=123')
-    assert.equal(f('x=abc'), null, 'x=abc')
-    assert.equal(f('x=33qrs'), null, 'x=33qrs')
-    assert.equal(f('3x=33'), null, '3x=33')
-    assert.equal(f('beep x=123123 boop'), '123123', 'beep x=123123 boop')
-  })
+        assert.equal(f('x=5'), '5', 'x=5')
+        assert.equal(f('abc x=5'), '5', 'abc x=5')
+        assert.equal(f('fox=123'), null, 'fox=123')
+        assert.equal(f('x=abc'), null, 'x=abc')
+        assert.equal(f('x=33qrs'), null, 'x=33qrs')
+        assert.equal(f('3x=33'), null, '3x=33')
+        assert.equal(f('beep x=123123 boop'), '123123', 'beep x=123123 boop')
+    })
 
   it(`匹配8位 hex 代码，以'0x'开头，后面跟着两个字符可以是大写'A-F'，小写'a-f'，或者任意数字`, function () {
     const f = require('../lib/quantified_group')
@@ -68,7 +68,7 @@ describe('regex', function () {
 
   it(`将'@@...@@'markdown语法变成'<blink>...</blink>'`, function () {
     const f = require('../lib/blink')
-    
+
     assert.equal(
       f('@@whatever@@').trim(),
       '<p><blink>whatever</blink></p>',
